a2b20056c0a38715b1b4a4bdaa4cb0045d1ae799
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-05-13 19:20:23 +0200

    Creation of the tokenizer function

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"./tokenizer"
+)
 
 func main()  {
-	fmt.Println("Hello World!")
+	tokenizer.Tokenize("Et salut toi, aimerais-tu une glace chocolatée ?")
 }
--- tokenizer/tokenizer.go
+++ tokenizer/tokenizer.go
@@ -1,1 +1,10 @@
 package tokenizer
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Tokenize(token string){
+	fmt.Printf("%q\n", strings.Split(token, " "))
+}
