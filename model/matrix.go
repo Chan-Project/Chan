842d86d69796be4a10550b02da8f77296e84039a
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2020-05-27 23:16:16 +0200

    Putting all in float64

--- model/matrix.go
+++ model/matrix.go
@@ -1,8 +1,8 @@
 package model
 
-type Matrix []float32
+type Matrix []float64
 
-func Dot(a, b Matrix) (sum float32) {
+func Dot(a, b Matrix) (sum float64) {
 	for i := 0; i < len(b); i++ {
 		sum += a[i] * b[i]
 	}
